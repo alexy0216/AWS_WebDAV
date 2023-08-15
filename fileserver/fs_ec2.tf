b573f00291347e6ffe2e40958ea1e616223522db
alexy0216 <alexy0216@gmail.com>
2023-08-15 14:47:46 +0900

    Basic認証関連削除

--- fileserver/fs_ec2.tf
+++ fileserver/fs_ec2.tf
@@ -1,8 +1,3 @@
-# ！変更推奨！パスワード（ハッシュ値）
-variable "encoded_password" {
-  default = ":$2y$10$H56m.GlpN3F1VzQpSC9zoefQtyTNdqA.Z0wExeU9p7qWzefNixv1i"  # htpasswd -bnBC 10 "" password の結果を設定
-}
-
 # EC2インスタンスの作成
 resource "aws_instance" "fileserver_web_server" {
   ami                    = "ami-044dbe71ee2d3c59e" # Amazon Linux 2 Kernel 5.10 AMI 2.0.20230808.0 x86_64 HVM gp2
@@ -11,7 +6,7 @@ resource "aws_instance" "fileserver_web_server" {
   subnet_id              = aws_subnet.fileserver_subnet.id
   vpc_security_group_ids = [aws_security_group.fileserver_sg.id]
 
-  key_name = "fs_id_rsa" # SSH接続用のキーペアの名前
+  #key_name = "fs_id_rsa" # SSH接続用のキーペアの名前
 
   user_data = <<-EOF
   #! /bin/bash
@@ -27,7 +22,6 @@ resource "aws_instance" "fileserver_web_server" {
   sudo chown -R apache:apache /var/lib/dav
   sudo systemctl start httpd
   sudo systemctl enable httpd
-  sudo sed -i 's/AddDefaultCharset UTF-8/AddDefaultCharset Off/' /etc/httpd/conf/httpd.conf
 
   # webdav.confの設定を追加
   sudo mkdir /var/www/html/webdav
@@ -41,20 +35,6 @@ resource "aws_instance" "fileserver_web_server" {
     Require all granted
     </Directory>
   CONF'
-
-  # .htaccessファイルの作成とBasic認証の設定を追加
-  sudo bash -c 'cat << CONF > /var/www/html/webdav/.htaccess
-  AuthType Basic
-  AuthName "Restricted Area"
-  AuthUserFile /etc/httpd/.htpasswd
-  Require valid-user
-  CONF'
-
-  # エンコードされたパスワードを.htpasswdファイルに追加
-  echo 'WSuser{var.encoded_password}' | sudo tee -a /etc/httpd/.htpasswd
-
-  # .htaccessを有効化
-  # sudo sed -i "/<Directory \"/var/www/html\">/,/</Directory>/ s/AllowOverride None/AllowOverride All/" /etc/httpd/conf/httpd.conf
 
   sudo systemctl restart httpd
 
